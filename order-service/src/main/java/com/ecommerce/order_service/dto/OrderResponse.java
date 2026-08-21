bdd068951846ed0874e8e9b6f54974bf01eda87c
WiiruxCruz <hialf.92@gmail.com>
2026-08-20 19:15:57 -0600

    Se agrega dto y mapper

---
+++ order-service/src/main/java/com/ecommerce/order_service/dto/OrderResponse.java
@@ -0,0 +1,18 @@
+package com.ecommerce.order_service.dto;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderResponse {
+	private Long id;
+	private String orderNumber;
+	private List<OrderLineItemsResponse> orderLineItemsList;
+}
---
+++ order-service/src/main/java/com/ecommerce/order_service/mapper/OrderMapper.java
@@ -0,0 +1,26 @@
+package com.ecommerce.order_service.mapper;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import com.ecommerce.order_service.dto.OrderLineItemsRequest;
+import com.ecommerce.order_service.dto.OrderLineItemsResponse;
+import com.ecommerce.order_service.dto.OrderRequest;
+import com.ecommerce.order_service.dto.OrderResponse;
+import com.ecommerce.order_service.model.Order;
+import com.ecommerce.order_service.model.OrderLineItems;
+
+@Mapper(componentModel = "spring")
+public interface OrderMapper {
+	
+	//@Mapping(target = "id", ignore = true)
+	Order toOrder(OrderRequest orderRequest);
+	
+	//@Mapping(target = "id", ignore = true)
+	OrderLineItems toOrderLineItems(OrderLineItemsRequest orderLineItemsRequest);
+	
+	OrderResponse toOrderResponse(Order order);
+	
+	OrderLineItemsResponse toOrderLineItemsResponse(OrderLineItems orderLineItems);
+	
+}
