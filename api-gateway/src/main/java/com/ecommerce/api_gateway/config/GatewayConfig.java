4e7a3c0c0b62f08dc2d1e8c7aaceaf91bc58a1b7
WiiruxCruz <hialf.92@gmail.com>
2026-08-31 17:10:10 -0600

    Se realiza configuracion de api gateway

---
+++ api-gateway/src/main/java/com/ecommerce/api_gateway/config/GatewayConfig.java
@@ -0,0 +1,19 @@
+package com.ecommerce.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+
+@Configuration
+public class GatewayConfig {
+	
+	@Bean
+	public RouteLocator routeLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("product-service", r -> r
+						.path("/api/v1/product/**")
+						.uri("lb://PRODUCT-SERVICE")
+				).build();
+	}
+}
